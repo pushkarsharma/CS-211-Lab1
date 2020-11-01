2b19c8e72065f084794a726004fe750230030180
Pushkar Sharma <pushkarsharma1994@gmail.com>
2020-10-31 22:02:06 -0700

    Fixing variable names

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -150,7 +150,7 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
 {
     /* add your code here */
     /* please just copy from your lab1 function optimal( ... ) */
-    int l;
+    int new_i, new_j, new_k, l;
     for (new_i= i; new_i< n; new_i+= 3)
     {
         for (new_j = j; new_j < n; new_j += 3)
