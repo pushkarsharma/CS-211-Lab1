de7950c8c8171c861c919ea0f515256dd10c757b
Pushkar Sharma <pushkarsharma1994@gmail.com>
2020-10-31 22:30:22 -0700

    Trying some changes

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -150,60 +150,62 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
 {
     /* add your code here */
     /* please just copy from your lab1 function optimal( ... ) */
-    int new_i, new_j, new_k, l;
-    for (new_i= i; new_i< n; new_i+= 3)
+    int i1, j1, k1, l;
+    for (i1 = i; i1 < (i + b > n ? n : (i + b)); i1 += 3)
     {
-        for (new_j = j; new_j < n; new_j += 3)
+        for (j1 = j; j1 < (j + b > n ? n : (j + b)); j1 += 3)
         {
-            register double c_00 = C[new_i* n + new_j];
-            register double c_01 = C[new_i* n + (new_j + 1)];
-            register double c_02 = C[new_i* n + (new_j + 2)];
+            register double c00 = C[i1 * n + j1];
+            register double c01 = C[i1 * n + (j1 + 1)];
+            register double c02 = C[i1 * n + (j1 + 2)];
 
-            register double c_10 = C[(new_i+ 1) * n + new_j];
-            register double c_11 = C[(new_i+ 1) * n + (new_j + 1)];
-            register double c_12 = C[(new_i+ 1) * n + (new_j + 2)];
+            register double c10 = C[(i1 + 1) * n + j1];
+            register double c11 = C[(i1 + 1) * n + (j1 + 1)];
+            register double c12 = C[(i1 + 1) * n + (j1 + 2)];
 
-            register double c_20 = C[(new_i+ 2) * n + new_j];
-            register double c_21 = C[(new_i+ 2) * n + (new_j + 1)];
-            register double c_22 = C[(new_i+ 2) * n + (new_j + 2)];
+            register double c20 = C[(i1 + 2) * n + j1];
+            register double c21 = C[(i1 + 2) * n + (j1 + 1)];
+            register double c22 = C[(i1 + 2) * n + (j1 + 2)];
 
-            for (new_k = k; new_k < n; new_k += 3)
+            for (k1 = k; k1 < ((k + b) > n ? n : (k + b)); k1 += 3)
             {
                 for (l = 0; l < 3; l++)
                 {
-                    register double a_0l = A[new_i* n + new_k + l];
-                    register double a_1l = A[(new_i+ 1) * n + new_k + l];
-                    register double a_2l = A[(new_i+ 2) * n + new_k + l];
+                    //accessing A column wise
+                    register double a_0l = A[i1 * n + k1 + l];       //row1
+                    register double a_1l = A[(i1 + 1) * n + k1 + l]; //row2
+                    register double a_2l = A[(i1 + 2) * n + k1 + l]; //row3
 
-                    register double b_l0 = B[(new_k + l) * n + new_j];
-                    register double b_l1 = B[(new_k + l) * n + new_j + 1];
-                    register double b_l2 = B[(new_k + l) * n + new_j + 2];
+                    //accessing B row wise
+                    register double b_l0 = B[(k1 + l) * n + j1];     //col1
+                    register double b_l1 = B[(k1 + l) * n + j1 + 1]; //col2
+                    register double b_l2 = B[(k1 + l) * n + j1 + 2]; //col3
 
-                    c_00 += a_0l * b_l0;
-                    c_01 += a_0l * b_l1;
-                    c_02 += a_0l * b_l2;
+                    c00 -= a_0l * b_l0;
+                    c01 -= a_0l * b_l1;
+                    c02 -= a_0l * b_l2;
 
-                    c_10 += a_1l * b_l0;
-                    c_11 += a_1l * b_l1;
-                    c_12 += a_1l * b_l2;
+                    c10 -= a_1l * b_l0;
+                    c11 -= a_1l * b_l1;
+                    c12 -= a_1l * b_l2;
 
-                    c_20 += a_2l * b_l0;
-                    c_21 += a_2l * b_l1;
-                    c_22 += a_2l * b_l2;
+                    c20 -= a_2l * b_l0;
+                    c21 -= a_2l * b_l1;
+                    c22 -= a_2l * b_l2;
                 }
             }
 
-            C[new_i* n + new_j] = c_00;
-            C[new_i* n + (new_j + 1)] = c_01;
-            C[new_i* n + (new_j + 2)] = c_02;
+            C[i1 * n + j1] = c00;
+            C[i1 * n + (j1 + 1)] = c01;
+            C[i1 * n + (j1 + 2)] = c02;
 
-            C[(new_i+ 1) * n + new_j] = c_10;
-            C[(new_i+ 1) * n + (new_j + 1)] = c_11;
-            C[(new_i+ 1) * n + (new_j + 2)] = c_12;
+            C[(i1 + 1) * n + j1] = c10;
+            C[(i1 + 1) * n + (j1 + 1)] = c11;
+            C[(i1 + 1) * n + (j1 + 2)] = c12;
 
-            C[(new_i+ 2) * n + new_j] = c_20;
-            C[(new_i+ 2) * n + (new_j + 1)] = c_21;
-            C[(new_i+ 2) * n + (new_j + 2)] = c_22;
+            C[(i1 + 2) * n + j1] = c20;
+            C[(i1 + 2) * n + (j1 + 1)] = c21;
+            C[(i1 + 2) * n + (j1 + 2)] = c22;
         }
     }
 }
@@ -238,72 +240,66 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
  **/
 int mydgetrf_block(double *A, int *ipiv, int n, int b)
 {
-    int ib, i, j, k, maxIndex;
-    double max, sum;
-    double *tempv = (double*) malloc(sizeof(double) * n);
-
+    int i, j, k, t, maxind, ib;
+    double max;
+    double *tempv = (double *)malloc(sizeof(double) * n);
     for (ib = 0; ib < n; ib += b)
     {
-        for (i = ib; i < ib+b && i < n; i++)
+        for (i = ib; i < ib + b && i < n; i++)
         {
-            maxIndex = i;
-            max = fabs(A[i*n + i]);
-            
-            int j;
-            for (j = i+1; j < n; j++)
+            maxind = i;
+            max = fabs(A[i * n + i]);
+
+            for (t = i + 1; t < n; t++)
             {
-                if (fabs(A[j*n + i]) > max)
+                if (fabs(A[t * n + i]) > max)
                 {
-                    maxIndex = j;
-                    max = fabs(A[j*n + i]);
+                    maxind = t;
+                    max = fabs(A[t * n + i]);
                 }
             }
+
             if (max == 0)
             {
-                printf("LU factorization failed: coefficient matrix is singular.\n");
+                printf("LUfactoration failed: coefficient matrix is singular\n");
                 return -1;
             }
             else
             {
-                if (maxIndex != i)
+                if (maxind != i)
                 {
-                    int temp = ipiv[i];
-                    ipiv[i] = ipiv[maxIndex];
-                    ipiv[maxIndex] = temp;
+                    int temps = ipiv[i];
+                    ipiv[i] = ipiv[maxind];
+                    ipiv[maxind] = temps;
 
-                    memcpy(tempv, A + i*n, n * sizeof(double));
-                    memcpy(A + i*n, A + maxIndex*n, n * sizeof(double));
-                    memcpy(A + maxIndex*n, tempv, n * sizeof(double));
+                    memcpy(tempv, A + i * n, n * sizeof(double));
+                    memcpy(A + i * n, A + maxind * n, n * sizeof(double));
+                    memcpy(A + maxind * n, tempv, n * sizeof(double));
                 }
-            }
 
-            for (j = i+1; j < n; j++)
-            {
-                A[j*n + i] = A[j*n + i] / A[i*n + i];
-                int k;
-                for (k = i+1; k < ib+b && k < n; k++)
+                for (j = i + 1; j < n; j++)
                 {
-                    A[j*n + k] -= A[j*n +i] * A[i*n + k];
+                    A[j * n + i] = A[j * n + i] / A[i * n + i];
+                    for (k = i + 1; k < ib + b && k < n; k++)
+                        A[j * n + k] = A[j * n + k] - A[j * n + i] * A[i * n + k];
                 }
             }
         }
-
-        for (i = ib; i < ib+b && i < n; i++)
+        for (i = ib; i < ib + b && i < n; i++)
         {
-            for (j = ib+b; j < n; j++)
+            for (j = ib + b; j < n; j++)
             {
-                sum = 0;
+                double sum = 0.0;
                 for (k = ib; k < i; k++)
                 {
-                    sum += A[i*n + k] * A[k*n + j];
+                    sum += A[i * n + k] * A[k * n + j];
                 }
-                A[i*n + j] -= sum;
+                A[i * n + j] -= sum;
             }
         }
-
-        for (i = ib+b; i < n; i += b)
+        for (i = ib + b; i < n; i += b)
         {
-            for (j = ib+b; j < n; j += b)
+            for (j = ib + b; j < n; j += b)
             {
                 mydgemm(A, A, A, n, i, j, ib, b);
             }
